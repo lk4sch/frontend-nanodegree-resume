c5ba9368621c4debcd46435f346b55f82fa03edc
lk4sch <lk4sch@gmail.com>
2015-10-01 17:49:16 -0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -14,6 +14,12 @@ replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
+var FormattedName = HTMLheaderName.replace("%data%" , "Laura Forsch");
+var FormattedRole = HTMLheaderRole.replace("%data%" , "Software Developer");
+("#header").prepend(FormattedName);
+("#header").prepend(FormattedRole)
+
+
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
